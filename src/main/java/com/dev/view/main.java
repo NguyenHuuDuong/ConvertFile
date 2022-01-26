a4ad0f9678b5cf73b6a3c2c5731f88d298d80197
NguyenHuuDuong <huuduongptit@gmail.com>
2022-01-26 09:50:17 +0700

    update code

--- src/main/java/com/dev/view/main.java
+++ src/main/java/com/dev/view/main.java
@@ -15,7 +15,7 @@ public class main extends JFrame{
     private JButton extractFileButton;
     private JButton cancelButton;
     private JButton selectFileButton;
-
+    private String localPath = "";
     public main() {
     setInit();
         selectFileButton.addActionListener(new ActionListener() {
@@ -36,7 +36,7 @@ public class main extends JFrame{
                     } // if the user cancelled the operation
                     else {
                         //l.setText("the user cancelled the operation");
-                        JOptionPane.showMessageDialog(null, "You don't select file");
+                        JOptionPane.showMessageDialog(null, "Please select a file to convert...");
                     }
                 } // if the user presses the open dialog show the open dialog
                 else {
@@ -52,42 +52,91 @@ public class main extends JFrame{
                     } // if the user cancelled the operation
                     else {
                         //l.setText("the user cancelled the operation");
-                        JOptionPane.showMessageDialog(null, "You don't select file");
+                        JOptionPane.showMessageDialog(null, "Please select a file to convert...");
                     }
                 }
                 //System.out.println(link);
                 txtSelectPath.setText(link);
+                localPath = link;
             }
         });
         extractFileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String path = txtSelectPath.getText();
+                String com = e.getActionCommand();
+                txtSelectPath.setText("");
+                String link = "";
+                if (com.equals("save")) {
+                    // create an object of JFileChooser class
+                    JFileChooser jFC = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+                    // invoke the showsSaveDialog function to show the save dialog
+                    int confirm = jFC.showSaveDialog(null);
+                    // if the user selects a file
+                    if (confirm == JFileChooser.APPROVE_OPTION) {
+                        // set the label to the path of the selected file
+                        link = jFC.getSelectedFile().getAbsolutePath();
+                    } // if the user cancelled the operation
+                    else {
+                        //l.setText("the user cancelled the operation");
+                        JOptionPane.showMessageDialog(null, "Please select folder and enter filename to save...");
+                    }
+                } // if the user presses the open dialog show the open dialog
+                else {
+                    // create an object of JFileChooser class
+                    JFileChooser jFC = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+                    // invoke the showsOpenDialog function to show the save dialog
+                    int confirm = jFC.showOpenDialog(null);
+                    // if the user selects a file
+                    if (confirm == JFileChooser.APPROVE_OPTION) {
+                        // set the label to the path of the selected file
+                        //l.setText(j.getSelectedFile().getAbsolutePath());
+                        link = jFC.getSelectedFile().getAbsolutePath();
+                    } // if the user cancelled the operation
+                    else {
+                        //l.setText("the user cancelled the operation");
+                        JOptionPane.showMessageDialog(null, "Please select folder and enter filename to save...");
+                    }
+                }
+                //System.out.println(link);
+                //txtSelectPath.setText(link);
+//                String path = txtSelectPath.getText();
+                String pathFileInput = localPath;
                 //Create a PdfDocument instance
                 PdfDocument pdf = new PdfDocument();
                 //Load a PDF file
-                pdf.loadFromFile(path);
+                pdf.loadFromFile(pathFileInput);
                 //Save to .docx file
-                pdf.saveToFile("ToWord.docx", FileFormat.DOCX);
+                String pathFileOutPut = link;
+                pdf.saveToFile( pathFileOutPut +".docx", FileFormat.DOCX);
                 pdf.close();
+                JOptionPane.showMessageDialog(null,"Convert Successfully");
             }
         });
         cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                   System.exit(0);
             }
         });
     }
     public void setInit(){
         MainView.setPreferredSize(new Dimension(500,200));
         MainView.setMinimumSize(new Dimension(500,200));
+//        add(MainView);
+//        setSize(1400, 800);
+//        setTitle("Manager tickets System");
+//        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//        this.setLocationRelativeTo(null); // set center frame
+//        this.setResizable(false); // not allow zoom frame
+
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("My tool");
         frame.setContentPane(new main().MainView);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
         frame.pack();
         frame.setVisible(true);
     }
